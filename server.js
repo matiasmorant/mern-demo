7e89ce868f3880fa16fdb6d0446d62c1a175dbd6
wvlia5 <wvlia5@live.com.ar>
2018-07-30 19:13:10 -0300

    auto-update

--- server.js
+++ server.js
@@ -5,49 +5,49 @@ const MongoClient = require('mongodb').MongoClient
 const path = require('path');
 
 MongoClient.connect('mongodb://db-user1:db-user1@ds245971.mlab.com:45971/people-demo')
-  .then(
-    (client) => {
-      const People = client.db("people-demo").collection("people")
-      const schema = buildSchema(`
-        type Query {
-          people(name: String = "", origin: String): [Person]
-        }
+	.then(
+		(client) => {
+			const People = client.db("people-demo").collection("people")
 
-        type Person {
-          _id: String
-          first_name: String
-          last_name: String
-          email: String
-          gender: String
-          photo: String
-          origin: String
-        }
+	    const schema = buildSchema(`
+	      type Query {
+	        people(name: String = "", origin: String): [Person]
+	      }
 
-        schema {
-          query: Query
-        }
-      `);
+	      type Person {
+	        _id: String
+	        first_name: String
+	        last_name: String
+	        email: String
+	        gender: String
+	        photo: String
+	        origin: String
+	      }
 
-      const resolvers = {
-          people: ({name, origin}) => {
-            re = name
-              .trim()
-              .replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&') // escape RegExp special characters
-              .replace(/\s+/, "|") // Interpret whitespace as an OR
-            re = RegExp(re,"i")
-            name_filter = re ? {$or: [{first_name: re}, {last_name: re}] } : {}
-            origin_filter = origin ? {origin: origin} : {}
-            console.log(name_filter,origin_filter)
-            return People.find(Object.assign(name_filter, origin_filter)).limit(10).toArray()
-          },
-      }
-      // ===============================================
+	      schema {
+	        query: Query
+	      }
+	    `);
+
+	    const resolvers = {
+	        people: ({name, origin}) => {
+        		re = name
+        			.trim()
+        			.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&') // escape RegExp special characters
+        			.replace(/\s+/, "|") // Interpret whitespace as an OR
+        		re = RegExp(re,"i")
+      			name_filter = re ? {$or: [{first_name: re}, {last_name: re}] } : {}
+	        	origin_filter = origin ? {origin: origin} : {}
+	        	console.log(name_filter,origin_filter)
+	          return People.find(Object.assign(name_filter, origin_filter)).limit(10).toArray()
+	        },
+	    }
+			// __________ create the server _______
       var app = express();
       // use graphql
       app.use('/graphql', graphqlHTTP({
         schema: schema,
         rootValue: resolvers,
-        graphiql: true,
       }));
       // serve frontend
       app.use(express.static(path.join(__dirname, 'build')));
@@ -55,9 +55,8 @@ MongoClient.connect('mongodb://db-user1:db-user1@ds245971.mlab.com:45971/people-
         res.sendFile(path.join(__dirname, 'build', 'index.html'));
       });
 
-      app.listen(process.env.PORT || 9000, function(){
+      app.listen(process.env.PORT || 4000, function(){
         console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
       });
-      // console.log('Running a GraphQL API server at localhost:4000/graphql');      
-    }
-  )
+		}
+	)
